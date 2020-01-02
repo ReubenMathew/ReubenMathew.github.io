6af8875655fad9312caed4766ef353c13e8184c9
Reuben Mathew <intelphysic@gmail.com>
2020-01-02 09:20:19 -0500

    completed flask prototype

--- flaskvue/app.py
+++ flaskvue/app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template, jsonify
+from flask_cors import CORS 
+import requests
+
 import easydbio
 
 app = Flask(__name__,
 			static_folder = './dist/static',
 			template_folder = './dist')
+cors = CORS(app, resources={r'/api/*': {'origins':'*'}})
+
 app.config.from_object('config')
 
 db = easydbio.DB({
   "database": app.config['DATABASE'],
   "token": app.config['TOKEN']
 })
+
 
 @app.route('/api/spotify')
 def db_call():
--- flaskvue/frontend/src/components/Home.vue
+++ flaskvue/frontend/src/components/Home.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
     <p>Home page</p>
+    <p>Currently Listening To: {{ track }}</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+	data () {
+	    return {
+	    	track: ''
+	    }
+  	}, methods: {
+		getTrack() {
+			this.track = this.getTrackFromBackend()
+		},
+		getTrackFromBackend() {
+		    const path = `http://localhost:5000/api/spotify`
+		    axios.get(path)
+		    .then(response => {
+		    	this.track = response.data.curr
+		    })
+		    .catch(error => {
+		    	console.log(error)
+		    })
+	  	}
+	}, created () {
+	    this.getTrack(),
+	    this.getTrackFromBackend()
+  }
+}
+
+</script>
